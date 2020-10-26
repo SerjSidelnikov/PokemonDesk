b9674321b0395867f04bcbe356ea8fdf056fb008
Serj Sidelnikov <serj2107@gmail.com>
2020-10-26 19:26:31 +0300

    HomeWork 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import cn from 'classnames';
+
+import './custom.css';
+import classes from './App.modules.scss';
+
+const App = () => {
+  return (
+    <div className={cn(classes.header, 'color')}>
+      Yes, we Did It! This is App component!
+    </div>
+  );
+};
+
+export default App;
