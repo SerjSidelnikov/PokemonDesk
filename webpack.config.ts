639fb9ecbfdef5a65a736586df7d514d5965955d
Serj Sidelnikov <serj2107@gmail.com>
2020-10-29 20:31:45 +0300

    Homework-2

--- webpack.config.ts
+++ webpack.config.ts
@@ -8,7 +8,7 @@ module.exports = {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
   },
   mode: NODE_ENV ? NODE_ENV : 'development',
-  entry: path.resolve(__dirname, 'src/index.js'),
+  entry: path.resolve(__dirname, 'src/index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
@@ -18,6 +18,7 @@ module.exports = {
       {
         test: /\.[tj]sx?$/,
         use: ['ts-loader'],
+        exclude: [/node_modules/],
       },
       {
         test: /\.(s*)css$/,
