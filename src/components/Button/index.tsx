817f29b3ddcd35ed3bedaabe6d34e528cb10366c
Serj Sidelnikov <serj2107@gmail.com>
2020-11-12 20:24:57 +0300

    Style page not found

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,14 +4,22 @@ import cn from 'classnames';
 import classes from './Button.module.scss';
 
 interface ButtonProps {
+  className?: string;
   size?: 'small' | 'default';
   color?: 'yellow' | 'blue';
   fullWidth?: boolean;
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick, fullWidth = false, color, size = 'default' }) => {
-  const classNames = cn(classes.root, {
+const Button: React.FC<ButtonProps> = ({
+  className,
+  children,
+  onClick,
+  fullWidth = false,
+  color,
+  size = 'default',
+}) => {
+  const classNames = cn(classes.root, className, {
     [classes.fullWidth]: fullWidth,
     [classes.yellow]: color === 'yellow',
     [classes.blue]: color === 'blue',
--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import { navigate } from 'hookrouter';
 
-import Layout from '../../components/Layout';
+import Button from '../../components/Button';
+
+import teamRocket from './assets/teamRocket.png';
 
 import classes from './NotFound.module.scss';
+import { LinkEnum } from '../../routes';
 
 const NotFound: React.FC = () => {
+  const handleBack = (): void => {
+    navigate(LinkEnum.HOME);
+  };
+
   return (
     <div className={classes.root}>
-      <Layout>not found</Layout>
+      <div className={classes.container}>
+        <div className={classes.wrapper}>
+          <img src={teamRocket} alt="Team Rocket" />
+        </div>
+
+        <p className={classes.text}>
+          The rocket team <span>has won this time.</span>
+        </p>
+
+        <Button onClick={handleBack} color="yellow" className={classes.button}>
+          Return
+        </Button>
+      </div>
     </div>
   );
 };
