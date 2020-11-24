7b5f1d250c4b7fff0e392d98efd0acda8847b750
Serj Sidelnikov <serj2107@gmail.com>
2020-11-24 14:04:44 +0300

    Доработал функцию getUrlWithParamsConfig и написал тесты

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
+import useData from '../../hooks/useData';
+import { IPokemon } from '../../interface/pokemons';
+
 export interface PokemonProps {
   id: string | number;
 }
 
 const Pokemon: React.FC<PokemonProps> = ({ id }) => {
-  return <div>This is pokemon id equal {id}</div>;
+  const { data, isLoading } = useData<IPokemon>('getPokemon', { id });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return <div>This is pokemon id equal {data?.name}</div>;
 };
 
 export default Pokemon;
--- src/utils/getUrlWithParamsConfig.ts
+++ src/utils/getUrlWithParamsConfig.ts
@@ -1,11 +1,23 @@
 import config from '../config';
 
-const getUrlWithParamsConfig = (endpointConfig: string, query: { [key: string]: string | number }) => {
-  return {
+const getUrlWithParamsConfig = (endpointConfig: string, query: any) => {
+  const url = {
     ...config.client.server,
     ...config.client.endpoint[endpointConfig].uri,
     query,
   };
+
+  url.pathname = Object.keys(query).reduce((acc, value) => {
+    if (acc.indexOf(`{${value}}`) !== -1) {
+      const result = acc.replace(`{${value}}`, query[value]);
+      delete query[value]; // eslint-disable-line
+      return result;
+    }
+
+    return acc;
+  }, url.pathname);
+
+  return url;
 };
 
 export default getUrlWithParamsConfig;
